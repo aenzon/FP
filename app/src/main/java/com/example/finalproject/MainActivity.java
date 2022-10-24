85f3dffd8f1d508ea059724cfb4f88d1c3361f37
rejgn <rmtgallego@gmail.com>
2022-10-24 22:37:42 +0800

    Updated MainActivity.java

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn;
+    Button btnRes;
+    Button btnView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRes = findViewById(R.id.btnReserve);
+        btnView = findViewById(R.id.btnView);
     }
 }
