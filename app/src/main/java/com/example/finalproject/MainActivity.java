a6ffe8bcab69c40277955d588b2e6c8c42c87051
rejgn <rmtgallego@gmail.com>
2022-10-24 22:49:30 +0800

    Updated MainActivity.java

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,19 +2,30 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnRes;
-    Button btnView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void showAct(View v){
+        String btnTxt;
+        btnTxt = ((Button) v).getText().toString();
 
-        btnRes = findViewById(R.id.btnReserve);
-        btnView = findViewById(R.id.btnView);
+        if(btnTxt.equals("Reserve")){
+            Intent i = new Intent(this,Terms.class);
+            startActivity(i);
+        }
+        else if(btnTxt.equals("View Reservation")){
+            Intent i = new Intent(this,Payment.class);
+            startActivity(i);
+        }
+
     }
 }
