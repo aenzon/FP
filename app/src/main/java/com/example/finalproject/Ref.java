f10f7fde34cde2a2f24750ad659f76bb9f3d7299
Arvin <arvin.poldo@gmail.com>
2022-10-25 10:37:44 +0800

    Updated ref and refnum both xml and class.

--- app/src/main/java/com/example/finalproject/Ref.java
+++ app/src/main/java/com/example/finalproject/Ref.java
@@ -1,15 +1,43 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 public class Ref extends AppCompatActivity {
+
+    TextView reference;
+    Button next;
+    public static int randomNumber;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ref);
+
+        reference= findViewById(R.id.refNumber);
+        next= findViewById(R.id.btnHome);
+
+        Random random = new Random();
+        randomNumber = random.nextInt(5000)+1;
+        reference.setText(String.valueOf(randomNumber));
+
+    }
+    public void showActivity(View v)
+    {
+        String btnHome;
+        btnHome = ((Button) v).getText().toString();
+
+        if(btnHome.equals("Okay")){
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
     }
 }
--- app/src/main/java/com/example/finalproject/RefNum.java
+++ app/src/main/java/com/example/finalproject/RefNum.java
@@ -1,15 +1,37 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RefNum extends AppCompatActivity {
 
+    EditText reference;
+    Button next;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refnum);
+
+        reference = findViewById(R.id.txtReference);
+        next = findViewById(R.id.btnNext);
+    }
+
+    public void showActivity(View v)
+    {
+        String btnHome;
+        btnHome = ((Button) v).getText().toString();
+
+        if(btnHome.equals("Next")){
+            Intent i = new Intent(this, Reserve.class);
+            startActivity(i);
+        }
     }
 }
+
