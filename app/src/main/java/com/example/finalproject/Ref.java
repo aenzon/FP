d1153251b50b33c49e62cbe1f3a0a3b575801504
Arvin <arvin.poldo@gmail.com>
2022-10-27 18:51:54 +0800

    Updated Ref (clipboard added)

--- app/src/main/java/com/example/finalproject/Ref.java
+++ app/src/main/java/com/example/finalproject/Ref.java
@@ -1,17 +1,23 @@
 package com.example.finalproject;
 
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Ref extends AppCompatActivity {
 
+    ImageButton clip;
     TextView reference;
     Button next;
 
@@ -20,17 +26,28 @@ public class Ref extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ref);
 
+        clip= findViewById(R.id.btnClip);
         reference= findViewById(R.id.refNumber);
         next= findViewById(R.id.btnHome);
-
 
         reference.setText(Payment.refNum);
 
         next.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                    Intent i = new Intent(Ref.this, MainActivity.class);
-                    startActivity(i);
+                Intent i = new Intent(Ref.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        clip.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE); //
+                ClipData clip = ClipData.newPlainText("Copy", reference.getText().toString());
+                clipboard.setPrimaryClip(clip);
+
+                Toast.makeText(Ref.this, "Copied to clipboard!", Toast.LENGTH_SHORT).show();
             }
         });
     }
