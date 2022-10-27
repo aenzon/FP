8e6be57513138a79877a4b516600ed301c314ebf
Arvin <arvin.poldo@gmail.com>
2022-10-27 17:57:36 +0800

    Updated RefNum Class

--- app/src/main/java/com/example/finalproject/Reserve.java
+++ app/src/main/java/com/example/finalproject/Reserve.java
@@ -1,5 +1,7 @@
 package com.example.finalproject;
 
+import static com.example.finalproject.RefNum.rm;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Reserve extends AppCompatActivity {
 
+    TextView name, number, ci, co, pax, room;
     Button home;
 
     @Override
@@ -19,7 +22,19 @@ public class Reserve extends AppCompatActivity {
         setContentView(R.layout.activity_reserve);
 
         home= findViewById(R.id.btnBTHome);
+        name= findViewById(R.id.txtGName);
+        number= findViewById(R.id.txtGNo);
+        ci= findViewById(R.id.txtGCi);
+        co= findViewById(R.id.txtGCo);
+        pax= findViewById(R.id.txtGPax);
+        room= findViewById(R.id.txtGRoom);
 
+        name.setText(String.valueOf(rm.getName()));
+        number.setText(String.valueOf(rm.getContact()));
+        ci.setText(String.valueOf(rm.getCkin()));
+        co.setText(String.valueOf(rm.getCkout()));
+        pax.setText(String.valueOf(rm.getGuests()));
+        room.setText(String.valueOf(rm.getRoom()));
     }
 
     public void showActivity(View v)
