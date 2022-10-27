75d1945ece9df3b46f648c627c62e9baf3959259
krishadn <krizziasantillan@gmail.com>
2022-10-27 17:23:07 +0800

    Updated DAOReservation.java

--- app/src/main/java/com/example/finalproject/DAOReservation.java
+++ app/src/main/java/com/example/finalproject/DAOReservation.java
@@ -22,27 +22,8 @@ public class DAOReservation {
         return dbRef.child(id).setValue(rm);
     }
 
-    public ReservationModel get(String id){
-        ReservationModel rm = new ReservationModel();
-        dbRef.child(id).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if(task.isSuccessful()){
-                    if(task.getResult().exists()){
-                        DataSnapshot dataSnapshot = task.getResult();
-                        rm.setId(id);
-                        rm.setName(String.valueOf(dataSnapshot.child("name").getValue()));
-                        rm.setContact(String.valueOf(dataSnapshot.child("contact").getValue()));
-                        rm.setGuests(String.valueOf(dataSnapshot.child("guests").getValue()));
-                        rm.setCkin(String.valueOf(dataSnapshot.child("ckin").getValue()));
-                        rm.setCkout(String.valueOf(dataSnapshot.child("ckout").getValue()));
-                        rm.setRoom(String.valueOf(dataSnapshot.child("room").getValue()));
-                        rm.setStay((int)dataSnapshot.child("stay").getValue());
-                    }
-                }
-            }
-        });
-        return rm;
+    public Task<DataSnapshot> get(String id){
+        return dbRef.child(id).get();
     }
 
 }
